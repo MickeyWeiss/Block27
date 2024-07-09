1f49b4bfcf68032b084d17fb458b22c97b60099b
MickeyWeiss <167358315+MickeyWeiss@users.noreply.github.com>
2024-07-08 20:49:49 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Authenticate from './components/Authenticate.jsx'
+import SignUpForm from './components/SignUpForm.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [token, setToken] = useState(null);
   return (
     <>
-
+    <signUpForm token={token} setToken={setToken} />
+    <Authenticate token={token} setToken={setToken} />
     </>
   )
 }
